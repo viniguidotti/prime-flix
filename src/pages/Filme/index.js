17771c65f0ed346e769094293ac2a9944fedb04d
Guidotti <vinicius@cliqx.com.br>
2022-06-09 14:27:27 -0300

    First Commit

---
+++ src/pages/Filme/index.js
@@ -0,0 +1,11 @@
+function Filme(){
+    return(
+        <div>
+            <h1>
+                Bem Vindo á página Detalhes do Filme
+            </h1>
+        </div>
+    )
+}
+
+export default Filme;
---
+++ src/pages/Home/index.js
@@ -0,0 +1,11 @@
+function Home(){
+    return(
+        <div>
+            <h1>
+                Bem Vindo á Home
+            </h1>
+        </div>
+    )
+}
+
+export default Home;
